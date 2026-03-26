470265ad1c59df360b6bf32cb57a012c1021753d
Md Kazi Naim <kazinaim@softvence.com>
2026-03-26 13:04:58 +0600

    update template

--- src/modules/Auth/auth.route.ts
+++ src/modules/Auth/auth.route.ts
@@ -1,5 +1,20 @@
-import { Router } from "express";
+import { Router } from 'express';
+import validateRequest from '../../app/middleware/validateRequest';
+import { AuthValidation } from './auth.validation';
+import { AuthController } from './auth.controller';
 
-const router = Router()
+const router = Router();
 
-export const AuthRoute = router
+router.post(
+  '/register',
+  validateRequest(AuthValidation.registerValidationSchema),
+  AuthController.register
+);
+
+router.post(
+  '/login',
+  validateRequest(AuthValidation.loginValidationSchema),
+  AuthController.login
+);
+
+export const AuthRoute = router;
